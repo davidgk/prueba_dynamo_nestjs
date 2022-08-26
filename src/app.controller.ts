2f5cf2cfcecb6861c7aafe5395b8d30eddb715a0
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-08-26 11:21:46 -0300

    add default controller

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { APP_WELCOME } from './config/constants';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @Get()
+  welcome(): string {
+    return `<div style="padding: 50px"><h1 >${APP_WELCOME}</h1></br><h3>“Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away.” – Antoine de Saint-Exupery<h3></div>`;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { ConfigProjectModule } from './config/config.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [ConfigProjectModule, UserModule, TaskModule],
+  controllers: [AppController],
 })
 export class AppModule {}
