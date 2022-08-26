bb50ec574d69780d8a54854657f12db52847077b
matiasirpac <matiasirpac@gmail.com>
2022-08-26 08:40:57 -0300

    add adminjs without adapter for dynamo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigProjectModule } from './config/config.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
-
+import { AdminModule } from '@adminjs/nestjs';
+import { getModelToken, Model } from 'nestjs-dynamoose';
+import { User, UserKey } from './user/models/interfaces/user.interface';
 @Module({
-  imports: [ConfigProjectModule, UserModule, TaskModule],
+  imports: [
+    ConfigProjectModule,
+    AdminModule.createAdminAsync({
+      imports: [UserModule],
+      inject: [getModelToken('User')],
+      useFactory: (userModel: Model<User, UserKey>) => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          databases: [],
+        },
+      }),
+    }),
+    UserModule,
+    TaskModule,
+  ],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -14,6 +14,7 @@ import { UserService } from './services/user.service';
       },
     ]),
   ],
+  exports: [DynamooseModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
