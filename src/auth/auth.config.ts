e9a52d0f6fa13c1a068ae372fead255ef95d2dcd
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-09-19 18:39:21 -0300

    remove data

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthConfig {
-  public userPoolId = 'us-east-1_l7OfUeCNK';
-  public clientId = '20mous2beblg46arhu9k3a8dil';
+  public userPoolId = '';
+  public clientId = '';
   public region = 'us-east-1';
   public authority = `https://cognito-idp.${this.region}.amazonaws.com/${this.userPoolId}`;
 }
